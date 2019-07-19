4b376de8b3a7c9c0fc34e75ca41c6e73ed5754e6
Billy Arante <arantebillywilson@gmail.com>
2019-07-19 15:37:26 +0800

    add: Endpoint /api/exercise/new-user

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // /app.js
 
 'use strict';
+const bodyParser = require('body-parser');
+// models
+const User = require('./models/user');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
@@ -10,6 +13,8 @@ const express = require('express');
 const app = express();
 
 // middlewares
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors({ optionSuccess: 200 }));
 
@@ -29,6 +34,20 @@ app.get('/', (req, res, next) => {
     next();
 }, (req, res) => {
     res.send('app is now up and running');
+});
+
+let newUser;
+app.post('/api/exercise/new-user', (req, res, next) => {
+    newUser = new User({
+	"username": req.body.username
+    });
+    newUser.save();
+    next();
+}, (req, res) => {
+    res.json({
+	"username": newUser.username,
+	"_id": newUser.id
+    });
 });
 
 module.exports = app;
